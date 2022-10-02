58caceefe05f7393d50e4e0e4696f6717904be61
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2022-10-02 13:37:40 +0530

    feat: datetoStrConverter func

--- app.js
+++ app.js
@@ -10,3 +10,47 @@ function reverseStr(str){
 function isPalindrome(str){
    (str === reverseStr(str));
 }
+
+var dateStr = {
+date:'',
+month: '',
+year:''
+};
+
+
+function dateToStrConverter(date){
+
+    //day conversion
+
+    if(date.day<10){
+       dateStr.date = '0' + date.day;
+    }
+    else {
+        dateStr.date = date.day.toString();
+    }
+
+    //month conversion
+    if(date.month<10){
+        dateStr.month = '0' + date.month;
+    }
+    else {
+        dateStr.month = date.month.toString();
+    }
+
+    //yeat conversion
+    dateStr.year = date.year.toString();
+
+
+return dateStr;
+
+}
+
+
+//test date obj
+var date = {
+    day:4,
+    month: 10,
+    year: 2022
+}
+console.log(date);
+console.log(dateToStrConverter(date));
